dd5e97193be0005ec6455833d27ac281d3a2a925
emahaldar/em <emaparajitah@gmail.com/em>
2015-07-20 22:19:02 +0530

    Adding util.h

---
+++ util.h
@@ -0,0 +1,25 @@
+/* This software is distributed under the GNU General Public License Version 3 */
+/* Copyright (C) Aparajita Haldar */
+
+#ifndef EBDG_UTIL_H
+#define EBDG_UTIL_H
+
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <sstream>
+#include <time.h>
+
+extern const char* ndata_key;
+extern const char* edata_key;
+extern const char* name_key;
+
+extern bool DEBUG_MODE;
+
+int brand(int, int = 0);
+
+#endif	// EBDG_UTIL_H
+
+// Local variables:
+// mode: c++
+// End:
